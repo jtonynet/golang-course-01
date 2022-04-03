bbc40f64cab45c187769813da2454a1058105fc4
jtonyNet <jtony.net@gmail.com>
2022-04-03 19:19:45 -0300

    methods sacar and depositar in simple bank sample project

--- intermediate-bank-project/main.go
+++ intermediate-bank-project/main.go
@@ -9,10 +9,34 @@ type ContaCorrente struct {
 	saldo       float64
 }
 
-func main() {
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme", agencia: 589, numeroConta: 123456, saldo: 120.45}
-	contaDaVal := ContaCorrente{"Val", 222, 111222, 200.45}
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaVal)
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque Realizado com sucesso", c.saldo
+	}
+
+	return "Saldo insuficiente", c.saldo
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	}
+
+	return "valor do deposito menor do que zero", c.saldo
+
+}
+
+func main() {
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500.
+
+	fmt.Println(contaDaSilvia.saldo)
+
+	status, valor := contaDaSilvia.Depositar(2000.)
+	fmt.Println(status, valor)
 }
